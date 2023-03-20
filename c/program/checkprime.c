8eb38db534d10dcd1080815c7b5e44334b708168
jjw <ckjjw2953@naver.com>
2023-03-21 08:48:46 +0900

    modify code

--- c/program/checkprime.c
+++ c/program/checkprime.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 int checkprime (int x) {
   for (int i = 2; i < x; i++) {
     if (x == 2){
@@ -10,16 +9,3 @@ int checkprime (int x) {
   }
   return 0;
 }
-=======
-int checkprime (int x) {
-  for (int i = 2; i < x; i++) {
-    if (x == 2){
-      return 0;
-    }
-    else if (x % i == 0){
-      return 1;
-    }
-  }
-  return 0;
-}
->>>>>>> 74b5ff415b1ad3cb2579422e40d80d52580ecf21
