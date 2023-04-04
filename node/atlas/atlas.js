77d1b8bcd51c30bea3d3ce15f2cebad433dced61
wpdlf <ckjjw2953@naver.com>
2023-04-04 17:48:04 +0900

    atlas

---
+++ node/atlas/atlas.js
@@ -0,0 +1,28 @@
+var axios = require('axios');
+var data = JSON.stringify({
+    "collection": "testdb",
+    "database": "test",
+    "dataSource": "Cluster0",
+    "projection": {
+        "_id": 1
+    }
+});
+
+var config = {
+    method: 'post',
+    url: 'https://us-west-2.aws.data.mongodb-api.com/app/data-yroon/endpoint/data/v1/action/findOne',
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Request-Headers': '*',
+        'api-key': 'ON9d0m7MAa63L6U95uFnR3yyae4AYUaeSxMkF3kGpTZo9leXCAlOLT6n0kBbYT1c',
+    },
+    data: data
+};
+
+axios(config)
+    .then(function (response) {
+        console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
