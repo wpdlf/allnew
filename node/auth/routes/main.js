4e69fd1f95fd1ecd69af4974afe8b919182e9c5a
wpdlf <ckjjw2953@naver.com>
2023-04-11 15:25:15 +0900

    CRUD

--- node/auth/routes/main.js
+++ node/auth/routes/main.js
@@ -18,7 +18,61 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/hello', (req, res) => {
-    res.send('helllo world~!!')
+    res.send('Helllo World~!!')
+});
+
+// request 0, query 0
+app.get("/select", (req, res) => {
+    const result = connection.query("SELECT * FROM user");
+    console.log(result);
+    res.send(result)
+});
+
+// request 1, query 0
+app.post("/select", (req, res) => {
+    const result = connection.query("select * from user");
+    console.log(result);
+    res.send(result)
+});
+
+// request 1, query 1
+app.get("/selectQuery", (req, res) => {
+    const userid = req.query.userid;
+    const result = connection.query("select * from user where userid=?", [userid]);
+    console.log(result);
+    res.send(result)
+});
+
+// request 1, query 1
+app.post("/selectQuery", (req, res) => {
+    const userid = req.body.userid;
+    const result = connection.query("select * from user where userid=?", [userid]);
+    console.log(result);
+    res.send(result)
+});
+
+// request 1, query 1
+app.post("/insert", (req, res) => {
+    const { id, pw } = req.body;
+    const result = connection.query("insert into user values (?, ?)", [id, pw]);
+    console.log(result);
+    res.redirect('/selectQuery?userid=' + req.body.id);
+});
+
+// request 1, query 1
+app.post("/update", (req, res) => {
+    const { id, pw } = req.body;
+    const result = connection.query("update user set passwd=? where userid=?", [pw, id]);
+    console.log(result);
+    res.redirect('/selectQuery?userid=' + req.body.id);
+});
+
+// request 1, query 1
+app.post("/delete", (req, res) => {
+    const id = req.body.id;
+    const result = connection.query("delete from user where userid=?", [id]);
+    console.log(result);
+    res.redirect('/select');
 });
 
 module.exports = app;
