d36682b00fb7ec5bc3636f8d863855eb21cbe605
wpdlf <ckjjw2953@naver.com>
2023-04-25 09:59:08 +0900

    reauest

---
+++ node/http-request/post.js
@@ -0,0 +1,31 @@
+const { write } = require('fs');
+const https = require('https');
+
+const data = JSON.stringify({
+    todo: 'Buy the milk'
+})
+
+const options = {
+    hostname: '192.168.1.78',
+    port: 8000,
+    path: '/todos',
+    method: 'POST',
+    header: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length
+    }
+}
+
+const req = https.request(options, res => {
+    console.log(`statusCode : ${res.statusCode}`);
+    res.on('data', d => {
+        process.stdout.write(d);
+    })
+})
+
+req.on('error', error => {
+    console.log(error)
+})
+
+req.write(data)
+req.end()
