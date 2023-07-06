40af9344620f383622a33a140334cff2f97a94ca
wpdlf <ckjjw2953@naver.com>
2023-07-06 01:44:19 +0000

    study React

--- react/basic/src/App.js
+++ react/basic/src/App.js
@@ -1,33 +1,10 @@
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
-// import './App.css';
-
-// function App() {
-//   const name = 'Jiwon';
-//   const style = {
-//     backgroundColor : "green",
-//     color : 'aqua',
-//     fonsSize : 24,
-//     padding : '1rem'
-//   }
-//   return (
-//       <div>
-//         <Hello />
-//         <hr />
-//         <div style={style}>{name}</div>
-//         <div className="green-box"></div>
-//       </div>
-//     )
-// }
+import InputSample from './InputSample';
 
 function App() {
   return (
-      <Wrapper>
-      <Hello name="React" color="green" />
-      <Hello color="hotpink" />
-      </Wrapper>
-    )
+    <InputSample />
+  );
 }
 
 export default App;
